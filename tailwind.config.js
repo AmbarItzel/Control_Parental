7a86c30b0c9cd9b2688b6e08d4142ed57d9c7253
AmbarItzel <ambarcruz898@gmail.com>
2025-06-29 21:51:08 -0600

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx,html}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                mikrotik: {
-                    blue: '#1e40af',
-                    green: '#059669',
-                    red: '#dc2626'
-                }
-            }
-        },
-    },
-    plugins: [],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 }
